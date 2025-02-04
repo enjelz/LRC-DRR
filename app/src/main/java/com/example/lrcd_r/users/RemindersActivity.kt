223affcb9dde322d97cdeede1d65a668adac9635
Orenzzoo <lorenzadalem112@gmail.com>
2025-02-04 20:53:18 +0800

    Won't proceed if "I Agree" is not  checked| LRC D-RR

--- app/src/main/java/com/example/lrcd_r/users/RemindersActivity.kt
+++ app/src/main/java/com/example/lrcd_r/users/RemindersActivity.kt
@@ -3,6 +3,7 @@ package com.example.lrcd_r.users
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.CheckBox
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import com.example.lrcd_r.R
@@ -14,15 +15,23 @@ class RemindersActivity : DrawerBaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_reminders) // Provide layout resource ID
         activityRemindersBinding = ActivityRemindersBinding.inflate(layoutInflater)
+        setContentView(activityRemindersBinding.root)
         enableEdgeToEdge()
     }
-    fun btn_reminders_reserve(view: View){
-        val intent = Intent(this, ReceiptActivity::class.java)
-        startActivity(intent)
+
+    fun btn_reminders_reserve(view: View) {
+        val checkBox = findViewById<CheckBox>(R.id.reminders_i_agree)
+
+        if (checkBox.isChecked) {
+            val intent = Intent(this, ReceiptActivity::class.java)
+            startActivity(intent)
+        } else {
+            Toast.makeText(this, "You must agree to proceed.", Toast.LENGTH_SHORT).show()
+        }
     }
-    fun btnBack(view: View){
+
+    fun btnBack(view: View) {
         val intent = Intent(this, ConfirmationActivity::class.java)
         startActivity(intent)
     }
